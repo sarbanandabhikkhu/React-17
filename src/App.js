ee08d0fb7a175f5ec2607d0026caa3479ef3f5ef
sarbanandabhikkhu <sarbanandabhikkhu@gmail.com>
2022-09-25 19:56:35 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { hot } from "react-hot-loader/root";
 import Header from "./components/Header";
 import data from "../src/data/data.json";
+import logo from "./logo.svg";
 
 const App = () => {
   const { title, details } = data;
   return (
     <div className="App">
-      <img className="Logo" src="./logo.svg" />
+      <img className="Logo" src={logo} alt={title} />
       <Header title={title} details={details} />
     </div>
   );
