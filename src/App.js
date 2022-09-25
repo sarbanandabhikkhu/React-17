b97329a72fc7d3a7007fc6a6c437325e2c88288b
sarbanandabhikkhu <sarbanandabhikkhu@gmail.com>
2022-09-25 19:20:38 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
-// import Header from "./components/Header";
-// import data from "../src/data/data.json";
-// import "./App.scss";
+import Header from "./components/Header";
+import data from "../src/data/data.json";
 
 const App = () => {
-  // const { title, details } = data;
+  const { title, details } = data;
   return (
     <div className="App">
-      <img className="Logo" src="../logo.svg" />
-      {/* <Header title={title} details={details} /> */}
+      <img className="Logo" src="./logo.svg" />
+      <Header title={title} details={details} />
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,15 @@
 import React from "react";
 import Title from "./Title";
 import Details from "./Details";
-import "./Header.css";
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="Header">
-        <Title title={this.props.title} />
-        <Details details={this.props.details} />
-      </div>
-    );
-  }
-}
+const Header = (props) => {
+  const { title, details } = props;
+  return (
+    <div className="Header">
+      <Title title={title} />
+      <Details details={details} />
+    </div>
+  );
+};
 
 export default Header;
